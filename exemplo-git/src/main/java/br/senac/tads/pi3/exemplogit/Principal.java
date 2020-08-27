fd096563f03be1eb26e68d372ec43cd11f580a1a
biasj <beatriz1.sato@gmail.com>
2020-08-27 09:59:58 -0300

    Adiciona entrada de numero e operacao

--- exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
+++ exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
@@ -5,6 +5,8 @@
  */
 package br.senac.tads.pi3.exemplogit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author fedts
@@ -15,16 +17,22 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         
-        int teste1 = 0;
-        System.out.println("Valor teste1: " + teste1++);
+        System.out.print("Digite um número inteiro: ");
+        int teste1 = entrada.nextInt();
+        System.out.println("Valor teste1 + 1: " + teste1++);
+        
+        teste1 *= 210;
+        System.out.println("Teste1 x 210 = " + teste1);
         
         int teste2 = 0;
-        System.out.println("Valor teste1: " + ++teste2);
+        System.out.println("Valor teste1: " + --teste2);
         
         System.out.println("Exemplo Git");
-        System.out.println("Exemplo Commit 2");
         System.out.println("Quero causar conflito");
+        
+        
     }
     
 }
