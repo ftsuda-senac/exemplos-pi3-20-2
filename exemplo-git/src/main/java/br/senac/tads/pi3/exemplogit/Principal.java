6db342c6612a901796e36aaebdc33d0a267084b8
ftsuda-senac <ftsuda.senac@gmail.com>
2020-08-27 10:34:22 -0300

    fix #2

--- exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
+++ exemplo-git/src/main/java/br/senac/tads/pi3/exemplogit/Principal.java
@@ -17,6 +17,7 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
         
         System.out.print("Digite um número inteiro: ");
